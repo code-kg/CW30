b049dd9f317346be325da8c05b194cf2a50ccd6d
ASahanin <asahanin@gmail.com>
2019-12-26 20:44:44 +0600

    +++

--- src/Timur.java
+++ src/Timur.java
@@ -1,5 +1,5 @@
 public class Timur {
     public static void main(String[] args) {
-        System.out.println("Bulan branch with Timur");
+        System.out.println("Bulan branch with Timur and Andrei");
     }
 }
